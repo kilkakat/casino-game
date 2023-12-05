2b5864cd246c7863abd29fa6dc5e8902305cf490
kilkakat <149537659+kilkakat@users.noreply.github.com>
2023-12-05 15:13:28 +0200

    game.js

--- game.js
+++ game.js
@@ -14,3 +14,36 @@ class Baloon extends Circle {
         this.y = Math.random() * 100;
     }
 }
+
+
+// Получаем элемент canvas
+var canvas = document.getElementById('myCanvas');
+
+// Получаем контекст рисования (в данном случае, 2D)
+var ctx = canvas.getContext('2d');
+
+// Функция для получения координат курсора относительно canvas
+function getMousePos(canvas, event) {
+  var rect = canvas.getBoundingClientRect();
+  return {
+    x: event.clientX - rect.left,
+    y: event.clientY - rect.top
+  };
+}
+
+// Обработчик события mousemove
+canvas.addEventListener('mousemove', function(event) {
+  // Получаем координаты курсора
+  var mousePos = getMousePos(canvas, event);
+
+  // Очищаем canvas
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  // Рисуем круг на canvas в соответствии с координатами курсора
+  ctx.beginPath();
+  ctx.arc(mousePos.x, mousePos.y, 10, 0, 2 * Math.PI);
+  ctx.fillStyle = 'red';
+  ctx.fill();
+  ctx.stroke();
+});
+
