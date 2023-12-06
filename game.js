bd366e9bfc8acb7dfcc615ad4ac3cf27ef9d7d53
Катерина Кіласонія <kilkakat@MacBook-Air-Katerina.local>
2023-12-06 16:43:22 +0200

    add cursor to game.gs

--- game.js
+++ game.js
@@ -16,35 +16,12 @@ class Baloon extends Circle {
 }
 
 
-// Получаем элемент canvas
+// Get the canvas element
 var canvas = document.getElementById('myCanvas');
 
-// Получаем контекст рисования (в данном случае, 2D)
+// Get the 2D rendering context
 var ctx = canvas.getContext('2d');
 
-// Функция для получения координат курсора относительно canvas
-function getMousePos(canvas, event) {
-  var rect = canvas.getBoundingClientRect();
-  return {
-    x: event.clientX - rect.left,
-    y: event.clientY - rect.top
-  };
-}
-
-// Обработчик события mousemove
-canvas.addEventListener('mousemove', function(event) {
-  // Получаем координаты курсора
-  var mousePos = getMousePos(canvas, event);
-
-  // Очищаем canvas
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-  // Рисуем круг на canvas в соответствии с координатами курсора
-  ctx.beginPath();
-  ctx.arc(mousePos.x, mousePos.y, 10, 0, 2 * Math.PI);
-  ctx.arc(mousePos.x, mousePos.y, 10, 0, 2 * Math.PI);
-  ctx.fillStyle = 'red';
-  ctx.fill();
-  ctx.stroke();
-});
-
+// Draw a rectangle on the canvas
+ctx.fillStyle = 'red';
+ctx.fillRect(50, 50, 100, 75);
